745bf428ce3a57434d80a6c2253369dc97905b45
YuryMokriy <mavrandr@mail.ru>
2018-05-17 15:50:35 +0300

    Fix

--- lib/loss/seq2seq_crossentropy.py
+++ lib/loss/seq2seq_crossentropy.py
@@ -18,4 +18,4 @@ def cross_entropy(log_predictions, targets, alphabet, reduce_mean=True):
         return (log_predictions * mask
                 / (mask.sum(2, keepdim=True).sum(1, keepdim=True) * -log_predictions.size(0))).sum()
     else:
-        return (log_predictions * mask).sum(1).sum(1)
+        return -(log_predictions * mask).sum(1).sum(1)
